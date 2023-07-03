f24a0d64f493d6b347d3fb5be428154cd893e0be
Ciro Cancello <cirocancello75@gmail.com>
2023-07-03 12:23:10 +0200

    secondo commit

--- biblioteca/src/main/java/com/progettobiblioteca/biblioteca/BibliotecaApplication.java
+++ biblioteca/src/main/java/com/progettobiblioteca/biblioteca/BibliotecaApplication.java
@@ -1,4 +1,4 @@
-package com.biblioteca;
+package com.progettobiblioteca.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- biblioteca/src/test/java/com/progettobiblioteca/biblioteca/BibliotecaApplicationTests.java
+++ biblioteca/src/test/java/com/progettobiblioteca/biblioteca/BibliotecaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.biblioteca;
+package com.progettobiblioteca.biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
