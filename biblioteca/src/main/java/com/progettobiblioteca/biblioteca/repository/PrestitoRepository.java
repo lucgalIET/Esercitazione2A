4a8a1a3eca673c223566ddb41a66dc1073b5e71d
EnderModZe <manuel.arquati@gmail.com>
2023-07-04 12:47:54 +0200

    add dependecies

--- biblioteca/src/main/java/com/progettobiblioteca/biblioteca/repository/PrestitoRepository.java
+++ biblioteca/src/main/java/com/progettobiblioteca/biblioteca/repository/PrestitoRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-
 public interface PrestitoRepository extends JpaRepository<PrestitoEntity, Long> {
 
     @Query(value = "SELECT * " +
